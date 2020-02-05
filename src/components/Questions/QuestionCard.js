d332327cba14ed0f1bb2b6b746f31bbde7263469
Parth Shah <helloparthshah@gmail.com>
2020-02-05 00:44:46 -0800

    Green on true

--- src/components/Questions/QuestionCard.js
+++ src/components/Questions/QuestionCard.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { backend } from '../../apis/database'
 import '../css/QuestionPage-style.css'
-import AnswerBox from './AnswerBox'
-import QuestionList from './QuestionsList';
 
 class QuestionCard extends React.Component{
     constructor(props){
@@ -11,6 +9,7 @@ class QuestionCard extends React.Component{
             collapse: false,
             answer: '',
             useranswer: '',
+            correct:false
         }
         this.collapseSegment = this.collapseSegment.bind(this)
         
@@ -46,7 +45,7 @@ class QuestionCard extends React.Component{
     checkWin=(e)=>{
         if(e.key==="Enter"){
         if(this.state.answer==this.state.useranswer){
-            this.setState({answer:this.state.useranswer}) 
+            this.setState({correct:true}) 
             this.props.onHandleAnswer(this.state.useranswer, this.props.number)
             console.log("Found!!!"+this.props.number)
             alert("Correct!!")
@@ -60,16 +59,13 @@ class QuestionCard extends React.Component{
     render(){
         let containerState = !this.state.collapse ? `collapsible_bar_container_closed ${this.props.bar_color_closed}` :  `collapsible_bar_container_open ${this.props.bar_color_closed}`
         return (
-            <div className="question-bar_container">
+            <div className={this.state.correct==true?"question-bar_container_true":"question-bar_container"}>
                 <div className="space">
                     <div className={containerState} onClick={this.collapseSegment}>
                         <div className={`collapsible_bar_title `}>Question {this.props.number}</div>
                     </div>
                     {this.state.collapse ? (<div className={`collapsible_bar_content ${this.props.bar_color_open}`}>
-                        <input type="text" id="useranswer" placeholder="Enter Answer" value={this.state.useranswer} onChange={ (e) => this.handleAnswerOnChange(e) } onKeyDown={this.checkWin}/>
-                        {/* <button onClick={this.checkWin()}>Submit</button> */}
-                            {/* <input placeholder={this.state.answer}/> */}
-                            <p>Answer is: {this.state.useranswer}</p>
+                        <input className={'input'} type="text" id="useranswer" placeholder="Enter Answer" value={this.state.useranswer} onChange={ (e) => this.handleAnswerOnChange(e) } onKeyDown={this.checkWin}/>
                         </div>) : null} 
                 </div>
                 
