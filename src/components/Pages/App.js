12664b62be01c9db74146dff1a76e10843de61e5
Jose Mendoza <joseemendoza194@gmail.com>
2020-02-11 23:35:50 -0800

    Changed home page

--- src/components/Pages/App.js
+++ src/components/Pages/App.js
@@ -11,28 +11,18 @@ class App extends React.Component{
 
     render(){
         return(
-            <div className="grid-container_home">
-                <div className="grid-item_frame-1">1</div>
-                <div className="grid-item_frame-2">2</div>
-                <div className="grid-item_frame-3">3</div>
-                <div className="grid-item_frame-4">4</div>
-                <div className="grid-item_frame-5">5</div>
-                <div className="grid-item_center">
-                    <div className='button-container_home'>
+            <div className='grid-item_center'>
+                <div className='button-container_home'>
                         <Link to='/airbnb_questions'>
-                            <button className='button_home'>Start</button>
+                            <button className='button_home'>
+                                <span className='button-title'>Start</span>
+                            </button>
                         </Link>
                         
                     </div>
-                </div>
-                <div className="grid-item_frame-7">7</div>
-                <div className="grid-item_frame-8">8</div>
-                <div className="grid-item_frame-9">9</div>
-                <div className="grid-item_frame-10">10</div>
-                <div className="grid-item_frame-11">11</div>
-                <div className="grid-item_frame-12">12</div>
-                <div className="grid-item_frame-13">13</div>
             </div>
+            
+            
         )
     }
 }
